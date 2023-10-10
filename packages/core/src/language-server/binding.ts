b5eb259e89605b54324e11d74b76ed8b7b652c97
Cameron Dubas <camerondubas@gmail.com>
2023-10-10 12:49:54 +0100

    feat(lsp): add code-folding support

--- packages/core/src/language-server/binding.ts
+++ packages/core/src/language-server/binding.ts
@@ -29,6 +29,7 @@ export const capabilities: ServerCapabilities = {
   codeActionProvider: {
     codeActionKinds: [CodeActionKind.QuickFix],
   },
+  foldingRangeProvider: true,
   definitionProvider: true,
   workspaceSymbolProvider: true,
   renameProvider: {
@@ -233,5 +234,11 @@ export function bindLanguageServerPool({
 
       scheduleDiagnostics();
     });
+  });
+
+  connection.onFoldingRanges((params) => {
+    return pool.withServerForURI(params.textDocument.uri, ({ server }) =>
+      server.getFoldingRanges(params.textDocument.uri)
+    );
   });
 }
